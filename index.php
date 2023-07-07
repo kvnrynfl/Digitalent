25ab5724d51f0a091366ac8eed97b74f6b52eb29
kvnrynfl <kevinreynaufal2004@gmail.com>
2023-07-07 09:00:48 +0700

    Add link github

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Kevin Reynaufal</a>
+            <a class="navbar-brand" href="https://github.com/kvnrynfl/Digitalent">Kevin Reynaufal</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
